b41cd89bce21a34e002c281b61483a725b18d1e6
Hanieh Taherpour <153421656+haney-pie@users.noreply.github.com>
2023-12-25 11:41:56 +0330

    Add files via upload

---
+++ python practise /1/python session 4 .py
@@ -0,0 +1,21 @@
+x = int(input("Enter the number: "))
+
+i = 0
+
+while i<x:
+    print(i)
+    i = i+1
+
+while i>=0:
+    print(i)
+    i = i-1
+
+print("the end of the loop")
+
+# With For Loop
+
+for a in range(x):
+    print(a)
+    
+for i in range(x+1):
+    print(x-i)
