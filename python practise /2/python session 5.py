d4b3fa4d8a85d67cb5030147708570b7f5607ef0
Hanieh Taherpour <153421656+haney-pie@users.noreply.github.com>
2023-12-25 11:44:56 +0330

    Update python session 5.py

--- python practise /2/python session 5.py
+++ python practise /2/python session 5.py
@@ -1,13 +1,13 @@
-a = int(input("please enter the number of students : "))
+num_stu = int(input('Enter the number of your students: '))
 
-for i in range(a):
-    x = int(input('Enter the score: '))
-    
-    if 20>=x>=10:
-        print("Passed")
+for student in range(num_stu):
+    # score = int(input('Enter the score of' +  str(student+1) + 'th student: '))
+    score = int(input(f'Enter the score of {student+1}th student: '))
+    while score<0 or score>20:
+        print('Invalid input')
+        score = int(input(f'Enter the score of {student+1}th student again: '))
+    if score >= 10:
+        print(f'{student+1}th student passed')
     else:
-        if x>20 : 
-            print("not valide")
-        else:
-            print("failed")
+        print(f'{student+1}th student failed')
     
