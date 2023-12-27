935a4650b671b164830255c06378851ad38342ba
Hanieh Taherpour <153421656+haney-pie@users.noreply.github.com>
2023-12-27 11:38:38 +0330

    Add files via upload

--- python practise /2/python session 5.py
+++ python practise /2/python session 5.py
@@ -1,13 +1,13 @@
-num_stu = int(input('Enter the number of your students: '))
+a = int(input("please enter the number of students : "))
 
-for student in range(num_stu):
-    # score = int(input('Enter the score of' +  str(student+1) + 'th student: '))
-    score = int(input(f'Enter the score of {student+1}th student: '))
-    while score<0 or score>20:
-        print('Invalid input')
-        score = int(input(f'Enter the score of {student+1}th student again: '))
-    if score >= 10:
-        print(f'{student+1}th student passed')
+for i in range(a):
+    x = int(input('Enter the score: '))
+    
+    if 20>=x>=10:
+        print("Passed")
     else:
-        print(f'{student+1}th student failed')
+        if x>20 : 
+            print("not valide")
+        else:
+            print("failed")
     
